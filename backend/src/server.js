80c859fe5abbbb94fb71292cc2d10e0f2e2d3215
VictorG10 <victorgbadebo10@gmail.com>
2025-08-27 16:27:17 +0100

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,27 @@ import express from "express";
 import logger from "./middlewares/logger.middleware.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
 
 const PORT = ENV.PORT || 8081;
 
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
 // routes
 app.get("/", (req, res) => {
   res.send("<h1>X Native Clone</h1>");
 });
+
+app.use("/api/users", userRoutes);
 
 connectDB()
   .then(() =>
